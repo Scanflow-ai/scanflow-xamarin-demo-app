68d0f0db8a6371122ac4a89fbeb4cb80e102c8ea
Samson-Mychael <saravanask7175@gmail.com>
2023-06-09 16:07:26 +0530

    Forms changes
    Signed-off-by: Samson-Mychael <saravanask7175@gmail.com>

---
+++ Scanflow-Sample-XamarinForms/Scanflow/App.xaml.cs
@@ -0,0 +1,81 @@
+﻿using Scanflow.TextCapture.Xamarin.Forms;
+using Scanflow.Views;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+
+[assembly: ExportFont("HelveticaNowDisplay-Bold.ttf")]
+[assembly: ExportFont("HelveticaNowDisplay-Regular.ttf")]
+namespace Scanflow
+{ 
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+         
+
+            if (App.Current.Properties.ContainsKey("IsLandingPage"))
+            {
+                RequestCameraPermission();
+                MainPage = new NavigationPage(new HomePage());
+            }
+            else
+                MainPage = new NavigationPage(new WelcomePage());
+;
+        }
+
+       
+       
+        async void RequestCameraPermission()
+            {
+                var status = await Permissions.CheckStatusAsync<Permissions.Camera>();
+                if (status != PermissionStatus.Granted)
+                {
+                    await Permissions.RequestAsync<Permissions.Camera>();
+                }
+               
+            }
+       
+        protected override void OnStart()
+        {
+           
+        }
+       
+        protected override void OnSleep()
+        {
+            
+        }
+
+        protected override void OnResume()
+        {
+
+            MainPage = new NavigationPage(new HomePage());
+        }
+public static string AppTheme
+        {
+            get; set;
+        }
+
+        private void Application_RequestedThemeChanged(object sender, AppThemeChangedEventArgs e)
+        {
+            if (e.RequestedTheme == OSAppTheme.Dark)
+            {
+                App.AppTheme = "dark";
+                App.Current.Resources = new Styles.DarkTheme();
+            }
+
+            else
+            {
+                App.AppTheme = "white";
+                App.Current.Resources = new Styles.WhiteTheme();
+            }
+        }
+    }
+}
---
+++ Scanflow-Sample-XamarinForms/Scanflow/Views/WelcomePage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Scanflow.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WelcomePage : ContentPage
+    {
+        public WelcomePage()
+        {
+            InitializeComponent();
+        }
+        private void MainPageTapped(object sender, EventArgs e)
+        {
+            Application.Current.Properties["IsLandingPage"] = true;
+            Navigation.PushAsync(new HomePage());
+        }
+
+        private void CarouselView_CurrentItemChanged(object sender, CurrentItemChangedEventArgs e)
+        {
+            var view = sender as CarouselView;
+            if (view.Position == 0)
+            {
+                imgView.Source = "resource://Scanflow.Resources.skip1.svg";
+            }
+            else if (view.Position == 1)
+            {
+                imgView.Source = "resource://Scanflow.Resources.skip2.svg";
+            }
+            else if (view.Position == 2)
+            {
+                imgView.Source = "resource://Scanflow.Resources.skip3.svg";
+            }
+            else
+            {
+                imgView.Source = "resource://Scanflow.Resources.skip4.svg";
+            }
+        }
+    }
+}
